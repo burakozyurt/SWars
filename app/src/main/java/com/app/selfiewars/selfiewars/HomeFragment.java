bd4a4fd4d1502b455e89a244db1c0f99ad225a8d
Ahmet Burak Özyurt <ozyurtburak11@gmail.com>
2018-10-27 22:50:29 +0300

    Fragment Added BottomNavigation Bar added

---
+++ app/src/main/java/com/app/selfiewars/selfiewars/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.app.selfiewars.selfiewars;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+    Button button;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_home, container, false);
+        button = rootview.findViewById(R.id.button);
+
+        return rootview;
+    }
+
+}
--- app/src/main/java/com/app/selfiewars/selfiewars/MainActivity.java
+++ app/src/main/java/com/app/selfiewars/selfiewars/MainActivity.java
@@ -3,29 +3,78 @@ package com.app.selfiewars.selfiewars;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     ImageView ımageView;
     TextView textView;
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+    HomeFragment homeFragment;
+    RankFragment rankFragment;
+    SpinFragment spinFragment;
+    StoreFragment storeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tanimla();
-        intendAlma();
+        define();
+        setFrameLayout();
+
+       // intentAlma();
 
     }
+    public void setFrameLayout(){
+        homeFragment = new HomeFragment();
+        rankFragment = new RankFragment();
+        spinFragment = new SpinFragment();
+        storeFragment = new StoreFragment();
+        setFragment(homeFragment);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_rank:
+                        setFragment(rankFragment);
+                        return true;
+                    case R.id.nav_spin:
+                        setFragment(spinFragment);
+                        return true;
+                    case R.id.nav_store:
+                        setFragment(storeFragment);
+                        return true;
+                    default: return true;
+                }
+            }
+        });
+    }
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+    }
 
-    public void tanimla() {
+    public void define() {
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar);
+        frameLayout=findViewById(R.id.main_frame);
         ımageView = findViewById(R.id.selfiewarsLogo);
         textView = findViewById(R.id.selfiewarsText);
     }
 
-    public void intendAlma(){
+    public void intentAlma(){
         Intent ıntent = new Intent(this,AuthenticationScreen.class);
         startActivity(ıntent);
     }
