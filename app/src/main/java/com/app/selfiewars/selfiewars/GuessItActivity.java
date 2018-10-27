372154a42da9d7c2ffd18d380fc4881ab471dfb5
Gokhan <gkhnymc1@gmail.com>
2018-10-28 02:30:48 +0300

    Guess It In Activity added.

---
+++ app/src/main/java/com/app/selfiewars/selfiewars/GuessItActivity.java
@@ -0,0 +1,13 @@
+package com.app.selfiewars.selfiewars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessItActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_it);
+    }
+}
